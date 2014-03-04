e0a4b7ff26b24521991a264b13aea834865f0854
David Porter <contact@davidporter.id.au>
2014-03-05 02:06:31 +1000

    Stuff

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,30 @@
 'use strict';
 
 angular.module('davidporteridauApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .when('/contact', {
-        templateUrl: 'partials/contact',
-        controller: 'ContactCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-      
-    $locationProvider.html5Mode(true);
-  });
+		'ngCookies',
+		'ngResource',
+		'ngSanitize',
+		'ngRoute'
+		])
+.config(function ($routeProvider, $locationProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'partials/main',
+	controller: 'MainCtrl'
+	})
+	.when('/contact', {
+		templateUrl: 'partials/contact',
+	controller: 'ContactCtrl'
+	})
+	.when('/skills', {
+		templateUrl: 'partials/skills',
+	controller: 'SkillsCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+
+	$locationProvider.html5Mode(true);
+});
 
 
--- scripts/controllers/navbar.js
+++ scripts/controllers/navbar.js
@@ -2,10 +2,24 @@
 
 angular.module('davidporteridauApp')
   .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    $scope.menu = [
+	{
+	'title': 'Home',
+	'link': '/'
+	},
+	{
+	'title': 'Contact',
+	'link': '/contact'
+	},
+	{
+	'title': 'Skills & CV',
+	'link': '/skills'
+	},
+	{
+	'title': 'Github',
+	'link': 'https://github.com/DesertLynx'
+	},
+  ];
     
     $scope.isActive = function(route) {
       return route === $location.path();
